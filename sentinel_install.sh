2c6940ca3c3ea3a6424d0fbd13b84d44717c83e7
yonseiyes <yonseiyes@gmail.com>
2017-12-01 15:08:28 +0900

    Update sentinel_install.sh

--- sentinel_install.sh
+++ sentinel_install.sh
@@ -1,1 +1,7 @@
 #sentinel_install.sh
+git clone https://github.com/monacocoin-net/sentinel.git
+cd sentinel
+virtualenv ./venv
+./venv/bin/pip install -r requirements.txt
+vi sentinel.conf
+./venv/bin/py.test ./test

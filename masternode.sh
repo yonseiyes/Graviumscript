f171d527503d6d091b3f2211a25e6d9cb4efbdf8
yonseiyes <yonseiyes@gmail.com>
2018-06-15 14:15:27 +0900

    Update masternode.sh

--- masternode.sh
+++ masternode.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 # This script has been originally made by Gravium Coin Dev team. I changed only a few lines of original script.
+#Color codes
+RED='\033[0;91m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 clear
 cd ~
